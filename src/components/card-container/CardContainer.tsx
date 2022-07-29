c6326fb1899e1893904162e93ca8d3996bfab6fa
Gabriel Gosselin Roberge <gabrielgosrob@gmail.com>
2022-07-29 09:44:24 -0400

    add search bar

--- src/components/card-container/CardContainer.tsx
+++ src/components/card-container/CardContainer.tsx
@@ -1,18 +1,32 @@
 import Card from '../card/Card'
 import type { CardProps } from '../card/Card'
+import { ChangeEvent, useState } from 'react'
 
 export type CardContainerProps = {
   cards: CardProps[]
 }
 
 function CardContainer(props: CardContainerProps) {
-  const cards = getCards(props.cards)
+  const [search, setSearch] = useState('')
+  const cards = getCards(props.cards, search)
 
-  return <div className='flex flex-wrap gap-4 justify-around'>{cards}</div>
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value)
+  }
+
+  return (
+    <div className='flex flex-col gap-4'>
+      <input type='text' onChange={handleSearch} className='border-2' />
+      <div className='flex flex-wrap gap-4 justify-around'>{cards}</div>
+    </div>
+  )
 }
 
-function getCards(cards: CardProps[]) {
-  return cards.map((card, index) => <Card {...card} key={index} />)
+function getCards(cards: CardProps[], search: string) {
+  return cards.map((card, index) => {
+    if (card.name.toLowerCase().includes(search.toLowerCase()))
+      return <Card {...card} key={index} />
+  })
 }
 
 export default CardContainer
