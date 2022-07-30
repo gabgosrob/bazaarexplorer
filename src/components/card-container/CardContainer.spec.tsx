3ad5814eb53ef6852a81a8311c3e4bbf487b8018
Gabriel Gosselin Roberge <gabrielgosrob@gmail.com>
2022-07-30 04:12:10 -0400

    add search bar unit test

--- src/components/card-container/CardContainer.spec.tsx
+++ src/components/card-container/CardContainer.spec.tsx
@@ -1,23 +1,44 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 import CardContainer from './CardContainer'
 import type { CardProps } from '../card/Card'
 
-it('renders the cards', () => {
-  const card: CardProps = {
-    name: 'Sword',
-    image: 'placeholder.png',
-    cooldown: 10,
-    effect: 'Deal 10 damage.',
-    passive: 'Your other fire items charge 1.5x faster.',
-    cost: 5,
-    types: ['Weapon', 'Fire'],
-    size: 'medium',
-  }
+const sword: CardProps = {
+  name: 'Sword',
+  image: 'placeholder.png',
+  cooldown: 10,
+  effect: 'Deal 10 damage.',
+  passive: 'Your other fire items charge 1.5x faster.',
+  cost: 5,
+  types: ['Weapon', 'Fire'],
+  size: 'medium',
+}
+const blade: CardProps = {
+  name: 'Blade',
+  image: 'placeholder.png',
+  cooldown: 10,
+  effect: 'Deal 10 damage.',
+  passive: 'Your other fire items do some work!',
+  cost: 5,
+  types: ['Weapon'],
+  size: 'medium',
+}
 
-  const cards = [card, card, card]
+it('renders the cards', () => {
+  const cards = [sword, sword, sword]
 
   render(<CardContainer cards={cards} />)
 
-  expect(screen.getAllByText(card.name).length).toBe(cards.length)
+  expect(screen.queryAllByText(sword.name).length).toBe(cards.length)
+})
+
+it('shows the searched items', () => {
+  const cards = [sword, blade]
+
+  render(<CardContainer cards={cards} />)
+
+  const inputBox = screen.getByRole('textbox')
+  fireEvent.change(inputBox, { target: { value: sword.name } })
+
+  expect(screen.queryByText(blade.name)).toBeNull()
 })
