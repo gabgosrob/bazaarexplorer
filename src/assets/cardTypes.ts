92adf6fcf1c09402b0b1d8f26acee0e991daee6c
Gabriel Gosselin Roberge <gabrielgosrob@gmail.com>
2022-08-05 05:59:29 -0400

    add all card types

--- src/assets/cardTypes.ts
+++ src/assets/cardTypes.ts
@@ -7,3 +7,48 @@ export const neutralTypes = [
   'Monstrous',
   'Friend',
 ]
+
+export const pygmalienTypes = [
+  'Weapon',
+  'Gear',
+  'Vehicle',
+  'Property',
+  'Appliance',
+]
+
+export const vanessaTypes = ['Weapon', 'Aquatic', 'Property', 'Vehicle', 'Gear']
+
+export const julesTypes = [
+  'Meal',
+  'Ingredient',
+  'Gear',
+  'Weapon',
+  'Appliance',
+  'Food',
+  'Vehicle',
+]
+
+export const dooleyTypes = [
+  'Weapon',
+  'Friend',
+  'Ray',
+  'Attachment',
+  'Appliance',
+  'Vehicle',
+  'Tools',
+  'Core',
+  'Gear',
+]
+
+export const stelleTypes = [
+  'Drone',
+  'Tool',
+  'Vehicle',
+  'Weapon',
+  'Gear',
+  'Property',
+  'Weapon',
+  'Building',
+  'Meal',
+  'Appliance',
+]
